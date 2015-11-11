39121db7d107e728d307fb81a5877090c1a666dc
Freddy Tan <ftan@splunk.com>
2015-11-11 19:22:10 +0800

    1. refactor as OO style for installer 2. refactor states for is_installed checking

--- file_base/_states/splunk.py
+++ file_base/_states/splunk.py
@@ -1,6 +1,18 @@
 def installed(name, **kwargs):
-	'''
-	'''
-	__salt__['splunk.install'](**kwargs)
-	comment = "Splunk is installed"
-	return {'name': name, 'changes': {}, 'result': True, 'comment': comment}
+    ret = {'name': name,
+           'changes': {},
+           'result': True,
+           'comment': ''}
+
+    if __salt__['splunk.is_installed']():
+        ret['comment'] = 'Splunk is already installed.'
+        return ret
+
+    result = __salt__['splunk.install'](**kwargs)
+    if result:
+        ret['changes'] = {
+            'old': 'No splunk installed',
+            'new': result
+        }
+        ret['comment'] = "Splunk is installed"
+    return ret
