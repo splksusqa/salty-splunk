8ec1cb42aece3be6b21874e90f9a3c314cbd6a41
Wythe Lin <clin@splunk.com>
2016-04-13 14:54:00 +0800

    fix bugs on runner, update uf.sls

--- file_base/runners/splunk.py
+++ file_base/runners/splunk.py
@@ -15,21 +15,22 @@ def management_uri_list(role=None):
 
     return minions
 
+
 def get_forward_servers():
     '''
     Print a list of all of the minions that are up
     '''
     client = salt.client.LocalClient(__opts__['conf_file'])
     listening_ports = client.cmd(
-        "G@role:indexer", 'grains.get', 'listening_ports',
+        "G@role:indexer", 'grains.get', arg=['listening_ports'],
         expr_form='compound', timeout=300)
     ips = client.cmd(
-        "G@role:indexer", 'grains.get', 'ipv4',
+        "G@role:indexer", 'grains.get', arg=['ipv4'],
         expr_form='compound', timeout=300)
 
     ret = []
     for key, value in ips.items():
         ip = value[-1]
         for port in listening_ports[key]:
-            ret.append(ip + ":" + port)
-    return minions
+            ret.append(ip + ":" + str(port))
+    return ret
