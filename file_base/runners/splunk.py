4b3d6f69971bf4b8f29f67f14b282fe2430e18fb
Freddy Tan <ftan@splunk.com>
2016-05-24 14:09:15 +0800

    add log message

--- file_base/runners/splunk.py
+++ file_base/runners/splunk.py
@@ -1,6 +1,9 @@
 import salt.client
 import os
 import yaml
+import logging
+
+log = logging.getLogger(__name__)
 
 def management_uri_list(role=None):
     '''
@@ -15,6 +18,7 @@ def management_uri_list(role=None):
     # todo figure out how to avoid this hacky way but accelerated
     tsplk_info_path = '/home/ubuntu/tsplk_runner_info'
     if not os.path.exists(tsplk_info_path):
+        log.debug(tsplk_info_path + ' is not found.')
         minions = client.cmd(role_str, 'splunk.get_mgmt_uri', expr_form='compound',
                              timeout=300)
         return minions
@@ -27,4 +31,5 @@ def management_uri_list(role=None):
         if role in data['roles']:
             result.update({minion: data['private_ip'] + ':8089'})
 
+    log.warn('return list value: %s' % str(result))
     return result
