abd5686478a89d5daa7d08718496c45ccfbeaf44
Wythe Lin <clin@splunk.com>
2017-07-19 17:31:27 -0700

    windows is really slow... increasing timeout for minions to connect

--- file_base/runners/splunk.py
+++ file_base/runners/splunk.py
@@ -49,7 +49,7 @@ def join_ad_domain():
 
     log.warn('wait for vm {v}'.format(v=vm_count))
 
-    _wait_for_minions_to_connect(pillar['tsplk']['id_list'], 600)
+    _wait_for_minions_to_connect(pillar['tsplk']['id_list'], 900)
 
     return result
 
@@ -106,7 +106,7 @@ def create_site():
     runner.cmd('splunk.join_ad_domain')
     # from pillar list
     pillar = runner.cmd('pillar.show_pillar', [])
-    _wait_for_minions_to_connect(pillar['tsplk']['id_list'], 600)
+    _wait_for_minions_to_connect(pillar['tsplk']['id_list'], 900)
 
     if 'sites' in pillar:
         _clear_grains()
